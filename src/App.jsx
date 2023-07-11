c4ce0bef6d3d80d1e0aa8bee867751a3517e90ba
grinker <gabriele.barberio@gmail.com>
2023-07-11 17:11:18 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Es1 } from "./component/Es1/Es1";
+
+class App extends React.Component {
+  render() {
+    return (
+      <>
+        <Es1 />
+      </>
+    );
+  }
+}
+
+export default App;
