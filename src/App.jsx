d38a83b48ccef4fb4e1835537f0086a0b2a15988
grinker <gabriele.barberio@gmail.com>
2023-07-12 22:20:23 +0200

    esercizio 9

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { Welcome5 } from "./component/Es8/Welcome5";
-import { Age } from "./component/Es8/Age";
+import { Welcome5 } from "./component/Es9/Welcome5";
+import { Age } from "./component/Es9/Age";
 
 //name={<strong>Luca</strong>} passato come props per l'e5
 class App extends React.Component {
   render() {
     return (
       <>
-        <Welcome5 age={<Age />} name={<strong>Luca</strong>} />
+        <Welcome5 age={64} name={<strong>Luca</strong>} />
       </>
     );
   }
