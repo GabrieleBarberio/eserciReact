9e83e367b8a9f6cc95f8fd8f1518af9e01caaf7c
grinker <gabriele.barberio@gmail.com>
2023-07-13 11:47:30 +0200

    esercizio 18

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Counter } from "./component/Es14/Counter";
 import { ClickCounter } from "./component/Es16/ClickCounter";
 import ClickTracker from "./component/Es17/ClickTracker";
+import InteractiveWelcome from "./component/Es18/InteractiveWelcome";
 //name={<strong>Luca</strong>} passato come props per l'e5
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
         <Counter initialValue={Number(10)} />
         <ClickCounter />
         <ClickTracker />
+        <InteractiveWelcome />
       </>
     );
   }
---
+++ src/component/Es18/InteractiveWelcome.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { Welcome } from "./Welcome6";
+
+class InteractiveWelcome extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+    };
+  }
+
+  handleInputChange = (event) => {
+    this.setState({ name: event.target.value });
+  };
+
+  render() {
+    const { name } = this.state;
+    return (
+      <div>
+        <input type="text" value={name} onChange={this.handleInputChange} />
+        <Welcome name={name} />
+      </div>
+    );
+  }
+}
+
+export default InteractiveWelcome;
