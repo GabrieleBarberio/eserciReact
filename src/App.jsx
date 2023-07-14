29925b3edd21bd0ef46d95aafed245d0940b12c3
grinker <gabriele.barberio@gmail.com>
2023-07-14 17:13:30 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import UncontrolledLogin from "./component/Es21/UncontrolledLogin";
 import TodoList from "./component/Es23/TodoList";
 import TodoList1 from "./component/Es25/TodoList1";
 import TodoList2 from "./component/Es26/TodoList2";
+import TodoList3 from "./component/Es27/TodoList3";
 //name={<strong>Luca</strong>} passato come props per l'e5
 class App extends React.Component {
   constructor(props) {
@@ -36,6 +37,17 @@ class App extends React.Component {
         <span>Todo es 25</span>
         <TodoList1 />
         <TodoList2 />
+        <TodoList3
+          render={(todos, handleDelete) => (
+            <ul>
+              {todos.map((todo, i) => (
+                <li key={i}>
+                  {todo} <button onClick={() => handleDelete(i)}>Delete</button>
+                </li>
+              ))}
+            </ul>
+          )}
+        />
       </>
     );
   }
---
+++ src/component/Es27/TodoList3.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+class TodoList3 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //dichiarazione state
+      todos: [],
+      inputValue: "",
+    };
+  }
+
+  handleInputChange = (event) => {
+    this.setState({ inputValue: event.target.value }); //handle dell'input per controllare
+  };
+
+  handleAddItem = () => {
+    const { inputValue, todos } = this.state;
+    if (inputValue !== "") {
+      // controllo che non vengano aggiunte task come stringhe vuote
+      this.setState({
+        todos: [...todos, inputValue], // aggiungo la nuova task senza sovrascrivere
+        inputValue: "", //resetto l'input
+      });
+    }
+  };
+
+  handleReset = () => this.setState({ todos: [], inputValue: "" });
+  handleDelete = (index) => {
+    const { todos } = this.state; //instazio todo in questo scope
+    const updatedTodos = todos.filter((_, i) => i !== index); // salvo in una variabile intermedia il nuovo array
+    this.setState({ todos: updatedTodos }); //aggiorno lo state dopo l'eliminazione
+  };
+  render() {
+    const { todos, inputValue } = this.state;
+
+    return (
+      <div>
+        <ul>
+          {todos &&
+            todos.map((todo, i) => {
+              return (
+                <li key={i}>
+                  {todo}{" "}
+                  <button onClick={() => this.handleDelete(i)}>Delete</button>
+                </li>
+              );
+            })}
+        </ul>
+        <div>
+          <input
+            type="text"
+            value={inputValue}
+            onChange={this.handleInputChange}
+          />
+          <button onClick={this.handleAddItem}>Add Todo</button>
+          <button onClick={this.handleReset}> Reset</button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default TodoList3;
