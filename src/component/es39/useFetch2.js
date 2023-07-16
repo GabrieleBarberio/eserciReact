b26301235fd6c5843d78fbe1179ff22200da9474
grinker <gabriele.barberio@gmail.com>
2023-07-16 15:42:03 +0200

    es39

---
+++ src/component/es39/useFetch2.js
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+
+export const useFetch = (username) => {
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+  const [err, setErr] = useState(null);
+
+  const fetchData = async () => {
+    setLoading(true);
+    setErr(null);
+    if (!data) {
+      try {
+        const response = await fetch(
+          `https://api.github.com/users/${username}`
+        );
+        if (response.ok) {
+          const res = await response.json();
+          setData(res);
+        }
+      } catch (error) {
+        console.log(error);
+        setErr(error);
+      } finally {
+        setLoading(false);
+      }
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return [data, setData, err, loading];
+};
